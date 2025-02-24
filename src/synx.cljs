e9ff5a666a28d860f2dc6ee8e2f79b14c702f820
Tamás Dezső <dezso.t.tamas@gmail.com>
2025-02-24 18:07:15 +0100

    Handle multiple input files in one go

--- src/synx.cljs
+++ src/synx.cljs
@@ -19,14 +19,23 @@
           false)  ;; Return false instead of exiting
       true)))  ;; Return true if valid input
 
-;; Command-line execution
 (defn -main [& args]
-  (if (not= 2 (count args))
-    (do (println "Usage: node synx.cls <grammar.ebnf> <input.txt>")
+  (if (< (count args) 2)
+    (do (.write js/process.stderr "Usage: node synx.cjs <grammar.ebnf> <input1.txt> [<input2.txt> ...]\n")
         (js/process.exit 1))
-    (let [is-valid (apply validate-input args)]
-      (if is-valid
-        (println "Valid input")
-        (do (js/process.exit 1))))))
+    (let [grammar-file (first args)
+          input-files (rest args)
+          results (doall (map (fn [input-file]
+                                (println "[ CHECK    ] " input-file)
+                                (let [valid? (validate-input grammar-file input-file)]
+                                  (if valid?
+                                    (println "[       OK ] " input-file)
+                                    (println "[    ERROR ] " input-file))
+                                  valid?)) 
+                              input-files))
+          all-valid? (every? true? results)]  ;; Check if all files passed
+      (if (not all-valid?) 
+        (js/process.exit 1)  ;; Exit with error if any file is invalid
+        (js/process.exit 0))))) ;; Exit with success if all files are valid
 
 (set! *main-cli-fn* -main)
