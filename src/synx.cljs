376e6dffc16fdfb330ab4c79c1346c6fe44fb1f7
Tamás Dezső <dezso.t.tamas@gmail.com>
2025-02-27 14:40:17 +0100

    Read stdin if no input file is given

--- src/synx.cljs
+++ src/synx.cljs
@@ -2,6 +2,7 @@
   (:require [instaparse.core :as insta]
             ["fs" :as fs]))
 
+
 (defn read-file [filename]
   (try
     (fs/readFileSync filename "utf-8")
@@ -9,34 +10,55 @@
       (println "Error reading file:" filename)
       (js/process.exit 1))))
 
+
+(defn read-stdin []
+  (let [stdin (.-stdin js/process)]
+    (set! (.-encoding stdin) "utf8")
+    (let [data (atom "")]
+      (.on stdin "data" #(swap! data str %))
+      (js/Promise.
+        (fn [resolve _reject]
+          (.on stdin "end" #(resolve @data)))))))
+
+
 (defn create-parser [grammar-file abnf?]
   (let [grammar (read-file grammar-file)]
-    (if abnf?
-      (insta/parser grammar :input-format :abnf)
-      (insta/parser grammar))))
+    (apply insta/parser grammar (when abnf? [:input-format :abnf]))))
 
-(defn validate-input [parser input-file]
+
+(defn validate-input [parser input]
+  (let [result (parser input)]
+       (if (insta/failure? result)
+          (do (println (insta/get-failure result))
+              false)
+          true)))
+
+
+(defn validate-input-file [parser input-file]
   (println "[ CHECK    ] " input-file)
-  (let [input (read-file input-file)
-        result (parser input)]
-    (if (insta/failure? result)
-      (do (println (insta/get-failure result))
-          (println "[    ERROR ] " input-file)
-          false)
-      (do (println "[       OK ] " input-file)
-          true))))
+  (if (validate-input parser (read-file input-file))
+    (do (println "[    ERROR ] " input-file)
+        false)
+    (do (println "[       OK ] " input-file)
+        true)))
+
 
 (defn -main [& args]
-  (if (< (count args) 2)
-    (do (.write js/process.stderr "Usage: node synx.cjs [--abnf] <grammar.xbnf> <input1.txt> [<input2.txt> ...]\n")
-        (js/process.exit 1))
-    (let [abnf? (some #(= % "--abnf") args)
-          files (remove #(= % "--abnf") args)
-          grammar-file (first files)
-          parser (create-parser grammar-file abnf?)
-          input-files (rest files)
-          results (doall (map #(validate-input parser %) input-files))
-          all-valid? (every? true? results)]  ;; Check if all files passed
-      (js/process.exit (if all-valid? 0 1)))))
+  (if (< (count args) 1)
+    (do (.write js/process.stderr "Usage: node synx.cjs [--abnf] <grammar-file> <input-file1> [<input-file2> ...]\n")
+        (.write js/process.stderr "       echo 'text' | node synx.cjs [--abnf] <grammar-file>\n\n")
+        (js/process.exit 1)))
+  (let [abnf? (some #(= % "--abnf") args)
+        files (remove #(= % "--abnf") args)
+        grammar-file (first files)
+        input-files (rest files)
+        parser (create-parser grammar-file abnf?)]
+       (if (empty? input-files)
+         (-> (read-stdin)
+             (.then (fn [stdin-input] (js/process.exit (validate-input parser stdin-input)))))
+         (let [results (doall (map #(validate-input-file parser %) input-files))
+              all-valid? (every? true? results)]
+              (js/process.exit (if all-valid? 0 1))))))
+
 
 (set! *main-cli-fn* -main)
